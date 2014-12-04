8a9c083031af988007126b3a9f644afd3158663d
柯均洁 <kjj3300@163.com>
2014-12-04 17:49:27 +0800

    add MUX_B

---
+++ MUX_B.vhd
@@ -0,0 +1,41 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library work;
+use work.common.ALL;
+
+entity MUX_B is 
+    Port ( Imm : in STD_LOGIC_VECTOR (15 downto 0);
+           Ry  : in STD_LOGIC_VECTOR (15 downto 0);
+           WriteData : in STD_LOGIC_VECTOR (15 downto 0);
+           ALUOut    : in STD_LOGIC_VECTOR (15 downto 0);
+           Op2Src    : in Op2SrcType;
+           ForwardB  : in ForwardBType;
+           Ret : out STD_LOGIC_VECTOR (15 downto 0)
+    );
+end MUX_B;
+
+architecture Behaviour of MUX_B is
+begin
+    process(Imm, Ry, WriteData, ALUOut, Op2Src, ForwardB)
+    begin
+        case ForwardB is
+            when ForwardB_Un =>
+                case Op2Src is
+                    when Op2Src_Imm =>
+                        Ret <= Imm;
+                    when Op2Src_Ry =>
+                        Ret <= Ry;
+                    when Op2Src_0 =>
+                        Ret <= ZERO;
+                    when others =>
+                    Ret <= HIGH_RESIST;
+                end case;
+            when ForwardB_WriteData =>
+                Ret <= WriteData;
+            when ForwardB_ALUout =>
+                Ret <= ALUOut;
+            when others =>
+                Ret <= HIGH_RESIST;
+        end case;
+    end process;
+end architecture ; -- Behaviour
--- common.vhd
+++ common.vhd
@@ -6,9 +6,11 @@ package common is
 -- constants
     constant ZERO : std_logic_vector(15 downto 0) := "0000000000000000";
     constant HIGH_RESIST : std_logic_vector(15 downto 0) := "ZZZZZZZZZZZZZZZZ";
--- Mux control signals    
+-- MUX control signals    
     type WBSrcType is (WBSRrc_ALURes, WBSrc_Mem);
     type MemDataType is (MemData_Rx, MemData_Ry);
     type PCSrcType is (PCSrc_PC1, PCSrc_B, PCSrc_Rx_0, PCSrc_Rx_1, PCSrc_T_0, PCSrc_Rx, PCSrc_RA);
     type Op1SrcType is (Op1Src_Rx, Op1Src_Ry, Op1Src_SP, Op1Src_Imm, Op1Src_IH, Op1Src_PC1);
+    type Op2SrcType is (Op2Src_Imm, Op2Src_Ry, Op2Src_0);
+    type ForwardBType is (ForwardB_Un, ForwardB_WriteData, ForwardB_ALUout);
 end common;
