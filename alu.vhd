9cb73255aba1bfe2abf98f83a873fabaf76cb1be
Bo Tian <dxmtb@163.com>
2014-12-05 09:05:38 +0800

    fix ALU

--- alu.vhd
+++ alu.vhd
@@ -7,64 +7,42 @@ use work.common.all;
 
 entity alu is
     port(
-        a  : in std_logic_vector(15 downto 0);
-        b  : in std_logic_vector(15 downto 0);
-        op : in std_logic_vector(3 downto 0);
-
-        zf : out std_logic;
-        sf : out std_logic;
-        c  : out std_logic_vector(15 downto 0)
+        Op1    : in  std_logic_vector(15 downto 0);
+        Op2    : in  std_logic_vector(15 downto 0);
+        ALUOp  : in  ALUOpType;
+        ALURes : out std_logic_vector(15 downto 0)
         );
 end alu;
 
 architecture Behavioral of alu is
 begin
-    process (a, b, op)
+    process (Op1, Op2, ALUOp)
         variable res : std_logic_vector(15 downto 0) := ZERO;
     begin
-        case op is
-            when ALU_ADD =>
-                res := a + b;
-            when ALU_SUB =>
-                res := a - b;
-            when ALU_AND =>
-                res := a and b;
-            when ALU_OR =>
-                res := a or b;
-            when ALU_XOR =>
-                res := a xor b;
-            when ALU_NOT =>
-                res := not(a);
-            when ALU_SLL =>
-                res := to_stdlogicvector(to_bitvector(a) sll conv_integer(b));
-            when ALU_SRL =>
-                res := to_stdlogicvector(to_bitvector(a) srl conv_integer(b));
-            when ALU_SLA =>
-                res := to_stdlogicvector(to_bitvector(a) sla conv_integer(b));
-            when ALU_SRA =>
-                res := to_stdlogicvector(to_bitvector(a) sra conv_integer(b));
-            when ALU_ROL =>
-                res := to_stdlogicvector(to_bitvector(a) rol conv_integer(b));
-            when ALU_ROR =>
-                res := to_stdlogicvector(to_bitvector(a) ror conv_integer(b));
-            when ALU_NEG =>
-                res := ZERO - a;
+        case ALUOp is
+            when ALUOp_Plus =>
+                res := Op1 + Op2;
+            when ALUOp_And =>
+                res := Op1 and Op2;
+            when ALUOp_Sub =>
+                res := Op1 - Op2;
+            when ALUOp_Or =>
+                res := Op1 or Op2;
+            when ALUOp_SLL =>
+                res := to_stdlogicvector(to_bitvector(Op1) sll conv_integer(Op2));
+            when ALUOp_SRA =>
+                res := to_stdlogicvector(to_bitvector(Op1) sra conv_integer(Op2));
+            when ALUOp_If_Less =>
+                if ieee.std_logic_unsigned."<" (Op1, Op2) then
+                    res := (others => '1');
+                else
+                    res := (others => '0');
+                end if;
             when others =>
                 null;
         end case;
 
-        c <= res;
-
-        if (res = ZERO) then
-            zf <= ZF_TRUE;
-            sf <= SF_FALSE;
-        elsif (conv_integer(res) < 0) then
-            zf <= ZF_FALSE;
-            sf <= SF_TRUE;
-        else
-            zf <= ZF_FALSE;
-            sf <= SF_FALSE;
-        end if;
+        ALURes <= res;
     end process;
-end Behavioral;
 
+end Behavioral;
